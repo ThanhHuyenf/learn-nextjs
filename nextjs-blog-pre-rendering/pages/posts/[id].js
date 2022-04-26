3f2740f419602fe2c03dd95fdfed23f7b38b5750
thanhhuyenf <huyenmotnang@gmail.com>
2022-04-26 14:45:49 +0700

    deploy blog

--- nextjs-blog-pre-rendering/pages/posts/[id].js
+++ nextjs-blog-pre-rendering/pages/posts/[id].js
@@ -1,31 +1,21 @@
-// import Layout from '../../components/layout'
-//
-// export default function Post() {
-//     return <Layout>
-//         <>
-//             <p>Thanh HUyen</p>
-//         </>
-//     </Layout>
-// }
-// export async function getStaticPaths() {
-//     // Return a list of possible value for id
-// }
-//
-// export async function getStaticProps({ params }) {
-//     // Fetch necessary data for the blog post using params.id
-// }
-
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from "next/head";
+import Date from "../../components/date";
 
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br />
             {postData.id}
             <br />
             {postData.date}
+            <br/>
+            <Date dateString={postData.date} />
         </Layout>
     )
 }
